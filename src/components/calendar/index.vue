7b4ce9717dc29b1bae1667bfd0517c18c668087e
AntoineMeunier52 <meunier.antoine52@gmail.com>
2025-06-26 22:43:31 +0200

    refractor code

--- src/components/calendar/index.vue
+++ src/components/calendar/index.vue
@@ -2,8 +2,7 @@
 import { ref } from "vue";
 import dayjs from "dayjs";
 
-import Year from "./Year.vue";
-import Month from "./Month.vue";
+import SelectMonthYear from "./SelectMontYear.vue";
 import Date from "./Date.vue";
 
 const selectedDateValue = ref(dayjs().date());
@@ -13,8 +12,7 @@ const selectedDateValue = ref(dayjs().date());
   <!-- v-bing="$attrs" => permet de bind les attribut lors de l'appel du composent dans le composent parent-->
   <div class="flex space-x-5" v-bind="$attrs">
     <div class="flex flex-col">
-      <Year />
-      <Month />
+      <SelectMonthYear />
       <Date />
     </div>
     <div class="w-1/2">
